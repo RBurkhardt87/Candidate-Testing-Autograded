e9fc41a158ff04b313726c679284b9c55ac90cc7
RBurkhardt87 <r.burkhardt.ibclc@gmail.com>
2024-06-27 22:45:43 -0500

    I still need to remove my thought comments

--- candidate-testing.js
+++ candidate-testing.js
@@ -44,11 +44,9 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  
-  
   //candidateAnswer = input.question("Who was the first American woman in space? ");
 
-//PART 2: REPLACE CODE WITH UPDATED CODE THAT ASKS ALL QUESTIONS IN THE ARRAY
+//PART 2.2: UPDATE CODE THAT ASKS ALL QUESTIONS IN THE ARRAY, NOT JUST A SINGLE QUESTION & STORES RESPONSES
  for (let i = 0; i < questions.length; i++) {
   candidateAnswers[i] = input.question(questions[i]);
  } 
@@ -58,31 +56,25 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  
 
-  
+//PART 2.3 PRINT CANDIDATE'S ANSWER AND THE CORRECT ANSWER 
+
 for (let i = 0; i < candidateAnswers.length; i++) {
   console.log(`You answered ${candidateAnswers[i]} and the correct answer was ${correctAnswers[i]}.`);
 }
+console.log("\n");
 
-  // if (candidateAnswers[i] === correctAnswers[i]) {console.log('Correct Answer');
+//UPDATED THIS CODE TO CHECK ALL THE ANSWERS IN THE ARRAY RATHER THAN SINGLE QUESTION
+  // if (candidateAnswer[i] === correctAnswer[i]) {console.log('Correct Answer');
   // }   else {console.log('Incorrect Answer');
   // }
   
+let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
+//PART 3.1 THIS IS FOR COMPARING CANDIDATE ANSWERS ARRAY WITH THE CORRECT ANSWERS ARRAY (CASE INSENSITIVE) 
+//NOT SURE IF I WAS SUPPOSED TO PRINT IT OUT OR NOT. I CAN REMOVE THIS PART SINCE BELOW ALSO COMPARES AND SPLITS
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-  // let correctlyAnswered;
-  // let numTotalQuestions;
-  // grade = (correctlyAnswered) / (numTotalQuestions) * 100;
-
-  // function compareAnswers(candidateAnswers, correctAnswers) {
-   
-  let i = 0 
-  candidateAnswers[i];
-  correctAnswers[i];
-
+let i = 0 
     while (i < correctAnswers.length) {
       if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       console.log('Correct Answer!');
@@ -90,13 +82,32 @@ for (let i = 0; i < candidateAnswers.length; i++) {
       console.log('Incorrect Answer');
     } i++;
   }
+console.log("\n");
 
+//----------------------------------------------------------------------------------------------------------
+//PART 3.2 CALCULATE THE CANDIDATES SCORE AS A PERCENTAGE
+//THIS SECTION IS FOR ME TO FIGURE OUT HOW TO FIND THE PERCENTAGE OF THE GRADE
+//IDEA: put them in their own arrays (correct & incorrect) and then use the arrays possibly for the equation
+let answeredCorrectArray = [];
+let answeredIncorrectArray = [];
 
-
-  return grade;
+for (let i = 0; i < candidateAnswers.length; i++) {
+  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+    answeredCorrectArray.push(candidateAnswers[i]);
+  } else {
+    answeredIncorrectArray.push(candidateAnswers[i]);
+  }
 }
+grade = answeredCorrectArray.length/correctAnswers.length*100
 
-
+//LET CANDIDATE KNOW IF THEY PASSED OR FAILED THE QUIZ:
+if (grade >= 80) {
+  console.log(`You have passed the quiz with ${grade}%, Congrats!!!`)
+} else {
+  console.log(`Unfortunately you have failed the quiz with ${grade}%. Better luck next time.`)
+}
+  return grade; 
+}
 
 
 function runProgram() {
